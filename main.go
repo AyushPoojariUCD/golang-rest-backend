07fdd9114d9ad4041b60cee3fdee9f7003e07048
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2026-03-11 22:26:38 +0530

    feat: models setup goland backend

--- main.go
+++ main.go
@@ -3,15 +3,43 @@ package main
 import (
 	"net/http"
 
+	"go-rest-backend/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvents)
+
 	server.GET("/hello", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, World!")
 	})
 
 	server.Run(":8080")
+}
+
+func getEvents(c *gin.Context) {
+	events := models.GetAllEvents()
+	c.JSON(http.StatusOK, events)
+}
+
+func createEvents(c *gin.Context) {
+	var event models.Event
+
+	err := c.ShouldBindJSON(&event)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Event created successfully",
+		"event":   event,
+	})
 }
---
+++ models/event.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int       `json:"id"`
+	UserID      int       `json:"user_id"`
+	Title       string    `json:"title" binding:"required"`
+	Description string    `json:"description" binding:"required"`
+	Location    string    `json:"location" binding:"required"`
+	DateTime    time.Time `json:"date_time" binding:"required"`
+}
+
+var events = []Event{}
+
+func (e Event) Save() {
+	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
